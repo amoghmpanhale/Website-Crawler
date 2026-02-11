6c6321d1f45fa75e9ce8f90e816d50ca208482bd
amoghpanhale <amoghmpanhale@gmail.com>
2026-02-11 11:12:25 -0700

    Crawler endpoint works

---
+++ app/logging_config.py
@@ -0,0 +1,12 @@
+import logging
+import sys
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        stream=sys.stdout,
+    )
+    return logging.getLogger("rag_api")
+
+logger = setup_logging()
--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
+import time
 from fastapi import FastAPI
-from app.routes import auth_routes, collections, crawl
+from app.logging_config import logger
+from crawl4ai.utils import configure_windows_event_loop
+from app.routes import auth_routes
+from app.routes import collections
+from app.routes import crawl
+
+# crawl4ai function that configures windows to use ProactorEventLoop which is required for async subprocesses used in 
+# crawling. This should be called at the very start of the program before any async code runs. Otherwise it will use 
+# SelectorEventLoop which does not support subprocesses and will cause the crawler to fail on Window.
+configure_windows_event_loop()
 
 app = FastAPI()
 
 app.include_router(auth_routes.router)
+logger.info("auth_routes loaded.")
+
 app.include_router(collections.router)
+logger.info("collections loaded.")
+
 app.include_router(crawl.router)
+logger.info(f"crawl loaded.")
 
 @app.get("/")
 async def root():
+    logger.info("Root endpoint hit")
     return {"message": "RAG API is running"}
